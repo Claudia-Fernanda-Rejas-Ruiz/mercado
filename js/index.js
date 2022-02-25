3d5eed3f6a9ef3911d7da6531fdd8c41f78efedb
Claudia-Fernanda-Rejas-Ruiz <claudia.rejas@usil.pe>
2022-02-25 10:51:33 -0500

    Se agregó reloj utilizando setTimeOut

--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ const documentReady = () => {
   bienvenidaNombre.innerHTML += nombre;
 }
 document.addEventListener("DOMContentLoaded",documentReady);
+
+function currentTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* Estableciendo el tiempo */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* Estableciendo el reloj */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime(); /* Iniciando el proceso */
